dcf8c7d249e7e67a8f9f660fabbf78ed7ccbb410
Jon Kirkman <jonkirkman@gmail.com>
2013-07-14 23:53:56 -0600

    added parsing notes; tinkered with subprocess arguments and response handling;

--- dartlint.py
+++ dartlint.py
@@ -8,34 +8,61 @@ import threading
 class DartLintCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
+        print('DARTLINT: run')
         name = self.view.file_name()
-        self.view.insert(edit, 0, "Hello, World!\n")
 
         working_directory = os.path.dirname(name)
-        print('--DARTLINT--> Working dir: %s' % working_directory)
+        print('  |-> Working dir: %s' % working_directory)
 
         # for now let's just use the working dir
         project_root = working_directory
-        print('--DARTLINT--> Project root: %s' % project_root)
+        print('  |-> Project root: %s' % project_root)
 
-        print('--DARTLINT--> File to analyze: %s' % os.path.basename(name))
+        print('  |-> File to analyze: %s' % os.path.basename(name))
 
         settings = self.view.settings()
         dartsdk_path = settings.get('dartsdk_path')
 
         if not dartsdk_path:
-            print('--DARTLINT--> ERROR: Cannot find Dart SDK')
+            print('  |-> ERROR: Cannot find Dart SDK')
             dartsdk_path = '/Users/jonkirkman/Development/dart/dart-sdk'
 
-        analyzer_path = os.path.join(dartsdk_path, 'bin', 'dartanalyzer')
-        result = subprocess.check_output([analyzer_path, "-h"], universal_newlines=True)
-        print('--DARTLINT--> CMD: %s' % result)
+        # analyzer_path = os.path.join(dartsdk_path, 'bin', 'dartanalyzer')
+        args = [
+            os.path.join(dartsdk_path, 'bin', 'dartanalyzer'),
+            '--machine',
+            '--package-root',
+            os.path.join(project_root, 'packages'),
+            name
+        ]
+        try:
+            subprocess.check_output(args, universal_newlines=True)
+        except subprocess.CalledProcessError as e:
+            print('returncode: %s' % e.returncode)
+            print('cmd: %s' % e.cmd)
+            print('output: %s' % e.output)
 
     def findProjectRoot():
-        print('--DARTLINT--> Find the project root')
+        print('DARTLINT: findProjectRoot')
 
 
 class AnalyzerThread(threading.Thread):
 
     def run(self):
-        print('DARTLINT-> ...')
+        print('...')
+
+    def parseResults():
+        # Using the --machine flag skips the summary and yields a single issue per line
+        # Each line should contain the following fields, delimited by `|`
+        # [0] severity
+        #     ERROR, WARNING, SUGGESTION
+        # [1] error.errorCode.type
+        #     HINT, COMPILE_TIME_ERROR, PUB_SUGGESTION, STATIC_WARNING,
+        #     STATIC_TYPE_WARNING, SYNTACTIC_ERROR
+        # [2] error.errorCode
+        # [3] source.fullName
+        # [4] location.lineNumber
+        # [5] location.columnNumber
+        # [6] length
+        # [7] error.message
+        print('...')
